c309ece09b9a63fa380d484660b187226eee71a3
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2022-12-18 16:20:44 +0300

    feat: add logic for script when we click and when we do not click

---
+++ piano.js
@@ -0,0 +1,47 @@
+let keys = Array.from(document.querySelectorAll('.key'));
+let keys_black = Array.from(document.querySelectorAll('.key_black'));
+
+const color = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+function randomColor() {
+  return `#${color[Math.floor(Math.random() * color.length)]}${
+    color[Math.floor(Math.random() * color.length)]
+  }${color[Math.floor(Math.random() * color.length)]}${
+    color[Math.floor(Math.random() * color.length)]
+  }${color[Math.floor(Math.random() * color.length)]}${
+    color[Math.floor(Math.random() * color.length)]
+  }`;
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'background-color') return;
+  e.target.classList.remove('playing');
+  e.target.style.backgroundColor = null;
+}
+
+function output(e) {
+  let audio;
+  let note;
+  if (e.shiftKey) {
+    audio = document.querySelector(`audio[data-key="shift+${e.keyCode}"]`);
+    note = document.querySelector(`div[data-key="shift+${e.keyCode}"]`);
+  } else {
+    audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    note = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  }
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+  note.classList.add('playing');
+
+  note.style.backgroundColor = randomColor();
+}
+
+keys.forEach((key) => key.addEventListener('mousedown', output));
+keys_black.forEach((key) => key.addEventListener('mousedown', output));
+
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+keys_black.forEach((key) => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', output);
